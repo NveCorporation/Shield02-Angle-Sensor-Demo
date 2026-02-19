2cb07f24967fc2daa758acafe001450a411ee232
Daniela <iso-apps@nve.com>
2026-02-19 17:17:51 -0600

    Add files via upload

--- ASR002/ASR002-Shield02.ino
+++ ASR002/ASR002-Shield02.ino
@@ -1,8 +1,8 @@
 //-------------------------------------------------------
 //| NVE Arduino Shield Angle/Rotation sensor demo
 //| ASR002-10 TMR SPI angle sensor
-//| Author - NVE Corporation, Sam Weber
-//| 9/9/2024
+//| Author - NVE Corporation, 
+//| Rev. 2/19/2026 by D. Baker
 //|
 //| Compatible breakout board: ASR002-10E-EVB01
 //|
@@ -25,10 +25,7 @@ TwoWire *i2c = &Wire; // I2C interface
 const int numLeds = 60; // Number of LEDs in the array
 
 // Define your array of LED coordinates as ordered pairs (x, y)
-struct LEDCoordinate {
-  int x;
-  int y;
-};
+struct LEDCoordinate {int x,y;};
 
 LEDCoordinate ledCoordinates[numLeds] = {
     {0, 8}, {0, 7}, {0, 6}, {0, 5}, {0, 4}, {0, 3}, {0, 2}, {0, 1}, {0, 0},
@@ -42,7 +39,7 @@ LEDCoordinate ledCoordinates[numLeds] = {
 
 int angle;
 int previousLedIndex = -1;
-int lastLedIndexes[5] = {-1, -1, -1, -1, -1};  // To store the last 5 LED indexes
+int lastLedIndexes[5] = {-1, -1, -1, -1, -1};  // To store the last five LED indexes
 bool clockwise = true;  // To track the rotation direction
 
 void setup() {
@@ -65,8 +62,7 @@ void setup() {
 
   ledmatrix.setLEDscaling(0xFF);
 
-  // Lower the global current to reduce overall brightness
-  ledmatrix.setGlobalCurrent(0x50);  // Reduced global current
+  ledmatrix.setGlobalCurrent(0x50);  //Reduced global current to reduce brightness
   Serial.print("Global current set to: ");
   Serial.println(ledmatrix.getGlobalCurrent());
 
@@ -74,37 +70,33 @@ void setup() {
 }
 
 void loop() {
-asr002Method();
+ASR002Procedure();
 }
-
-//ASR002 master method, holds the functionality of the LED
-//panel and the ASR002's SPI communication 
-void asr002Method(){
+//Reads the sensor and sets the LEDs
+void ASR002Procedure(){
     SPICommunication();
-  int ledIndex = angleToLED(angle);
+    int ledIndex = angle/60; // Convert angle to LED number
 
   if (ledIndex != previousLedIndex) {
     // Determine the rotation direction
     clockwise = (ledIndex > previousLedIndex) || (ledIndex == 0 && previousLedIndex == numLeds - 1);
     
-    // Update LEDs with continuous lighting
+    // Update LEDs
     updateComet(ledIndex);
     previousLedIndex = ledIndex; // Update the previous index
   }
-
-  delay(10); // Adjust delay for desired update speed
+  delay(1); // Adjust delay for desired update speed
 }
 
-
-//Easy SPI communication to the ASR002 
+// Easy SPI communication to the ASR002 
 void SPICommunication() {
   digitalWrite(10, LOW);  // Enable sensor communication
   
-  // Read angle from sensor
+// Read angle from sensor
   angle = (SPI.transfer(0)) << 8; // Send 0 for address angle; receive angle MSB 
   delayMicroseconds(3);   // Allow 3 us between address bytes 
-  angle |= SPI.transfer(0); // 2nd address byte (0 for read); receive angle LSB 
-  delayMicroseconds(15);  // Allow 10 us for next data 
+  angle |= SPI.transfer(0); // Second address byte (0 for read); receive angle LSB 
+  delayMicroseconds(15);  // Allow 15 us for next data 
 
   digitalWrite(10, HIGH); // Disable sensor communication
 
@@ -112,46 +104,27 @@ void SPICommunication() {
   Serial.println(angle);
 }
 
-
-//converts the current angle to what LED should be lit
-int angleToLED(int angle) {
-  // Normalize angle from 0-3600 to 0-3599
-  angle = angle % 3600;
-  
-  // Map angle to an LED index (0-59)
-  return angle / 60;
-}
-
 void updateComet(int ledIndex) {
-  // Turn off the oldest LED in the tail (only if the tail is fully populated)
+// Turn off the oldest LED in the tail (only if the tail is fully populated)
   if (lastLedIndexes[4] != -1) {
     int oldIndex = lastLedIndexes[4];
     ledmatrix.drawPixel(ledCoordinates[oldIndex].x, ledCoordinates[oldIndex].y, ledmatrix.color565(0, 0, 0));
   }
 
-  // Shift the last 5 LED indexes
+// Shift the last five LED indexes
   for (int i = 4; i > 0; i--) {
     lastLedIndexes[i] = lastLedIndexes[i - 1];
   }
   lastLedIndexes[0] = ledIndex;  // Store the current LED index
 
-  // Light up the LEDs based on the direction of movement with harsher dimming
+// Set LEDs based on the direction of movement with steeper dimming
   for (int i = 0; i < 5; i++) {
     if (lastLedIndexes[i] != -1) {  // Check if the index is valid
       int index = lastLedIndexes[i];
-      uint16_t brightness = map(i, 0, 4, 255, 10); // Harsher dimming, lower the minimum brightness
+      uint16_t brightness = map(i, 0, 4, 255, 10); // Steeper dimming, lower the minimum brightness
       ledmatrix.drawPixel(ledCoordinates[index].x, ledCoordinates[index].y, ledmatrix.color565(0, brightness, brightness)); // Green, Blue, Red order
     }
   }
-
-  // Update the LED matrix display
-  ledmatrix.show();
-}
-
-void turnOffAllQuadrants() {
-  for (int i = 0; i < numLeds; i++) {
-    int x = ledCoordinates[i].x;
-    int y = ledCoordinates[i].y;
-    ledmatrix.drawPixel(x, y, ledmatrix.color565(0, 0, 0)); // Turn off LED
-  }
+// Update the LEDs
+//  ledmatrix.show();
 }
